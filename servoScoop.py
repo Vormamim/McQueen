41df9d0e6a5b97614eb3ee168c629c3a0d9c9407
Vormamim <81339757+Vormamim@users.noreply.github.com>
2026-06-11 14:13:19 +1000

    Create servoScoop.py

---
+++ servoScoop.py
@@ -0,0 +1,13 @@
+from microbit import *
+
+# Initialize Maqueen Plus V2 communication
+i2c.init(freq=100000, sda=pin20, scl=pin19)
+
+while True:
+    # 1. Send S2 servo down to 0 degrees
+    i2c.write(0x10, bytes([0x15, 0]))
+    sleep(2000)
+    
+    # 2. Send S2 servo up to 90 degrees
+    i2c.write(0x10, bytes([0x15, 90]))
+    sleep(2000)
